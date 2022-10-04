c926393dfd5cd52b536316569982397b6bb7b26f
Flynn Foley-Clarke <mastare.flynn@gmail.com>
2022-10-05 00:11:31 +0100

    updated hero

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,27 +1,25 @@
 import React from "react";
-import Image from "next/image";
-import testerImage from "../../images/tester.jpg";
+// import Image from "next/image";
+// import testerImage from "../../images/tester.jpg";
 import Link from "next/link";
 const Hero = () => {
   return (
     <div className="hero min-h-screen bg-base-200">
       <div className="hero-content flex-col lg:flex-row-reverse">
-        <Image
-          height={400}
-          width={200}
-          alt="unsplashimg"
-          src={testerImage}
-          className="max-w-sm rounded-lg shadow-2xl"
-        />
         <div>
           <h1 className="text-5xl font-bold">Welcome to Tickettracker.io</h1>
-          <p className="py-6">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
+          <p className=" text-xl">
+            A New look at Team, Project and Ticket Management systems.
           </p>
-          <Link href={"/dashboard"}>
-            <button className="btn btn-primary">Get Started</button>
+          <section className="py-6 ">
+            <p className="text-base font-bold text-info">
+              This site is still currently in early development.
+            </p>
+          </section>
+          <Link href={"demo/dashboard"}>
+            <button className="btn btn-primary" disabled>
+              View a demo
+            </button>
           </Link>
         </div>
       </div>
