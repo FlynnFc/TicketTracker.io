b24c9060e8e28f549ecbe4725a585b7f34f295c6
Flynn Foley-Clarke <mastare.flynn@gmail.com>
2022-10-07 23:58:51 +0100

    new migration

---
+++ prisma/migrations/20221007225841_yeppers/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Comments" (
+    "name" TEXT NOT NULL,
+    "date" TEXT NOT NULL,
+    "avatar" TEXT NOT NULL,
+    "message" TEXT NOT NULL,
+    "id" TEXT NOT NULL PRIMARY KEY
+);
