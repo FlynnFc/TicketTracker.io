7f841867cd0d5c9cd726c37ce805c2afdfb0548b
Flynn Foley-Clarke <mastare.flynn@gmail.com>
2022-10-08 01:12:18 +0100

    planet scale linked

--- prisma/migrations/20221007135332_ticket_added/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Ticket" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "title" TEXT NOT NULL,
-    "description" TEXT NOT NULL,
-    "priority" TEXT NOT NULL,
-    "ticketType" TEXT NOT NULL,
-    "assignedTo" TEXT NOT NULL
-);
--- prisma/migrations/20221007225841_yeppers/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Comments" (
-    "name" TEXT NOT NULL,
-    "date" TEXT NOT NULL,
-    "avatar" TEXT NOT NULL,
-    "message" TEXT NOT NULL,
-    "id" TEXT NOT NULL PRIMARY KEY
-);
