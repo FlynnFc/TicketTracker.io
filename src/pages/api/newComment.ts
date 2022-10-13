4fcd5f7404124c3170136408be29a47dd3777d95
Flynn Foley-Clarke <mastare.flynn@gmail.com>
2022-10-14 00:54:45 +0100

    new endpoint

---
+++ src/pages/api/newComment.ts
@@ -0,0 +1,23 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+import { PrismaClient, Prisma } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method !== 'POST') {
+    return res.status(405).json({ message: 'Method not allowed' });
+  }
+
+  try {
+
+
+    const ticket: Prisma.CommentsCreateInput = JSON.parse(req.body);
+    const savedContact = await prisma.comments.create({data: ticket});
+    console.log(savedContact)
+    res.status(200).json(savedContact);
+  } catch (err) {
+    res.status(400).json({ message: err });
+  }
+};
