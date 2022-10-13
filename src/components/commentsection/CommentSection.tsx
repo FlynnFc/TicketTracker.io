3ca2420b5a737ec7e7e3896f472577057f66e4bc
Flynn Foley-Clarke <mastare.flynn@gmail.com>
2022-10-14 00:55:51 +0100

    Update CommentSection.tsx

--- src/components/commentsection/CommentSection.tsx
+++ src/components/commentsection/CommentSection.tsx
@@ -1,44 +1,57 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Commentinput from "../../commentinput/Commentinput";
 import Comment from "../comment/Comment";
 
-const CommentSection = () => {
+type commentProps = {
+  map(arg0: (el: commentProps) => JSX.Element): React.ReactNode;
+  avatar: string;
+  date: string;
+  id: string;
+  message: string;
+  name: string;
+  ticketId: string;
+};
+
+const CommentSection = (props: { id: any }) => {
+  const [comments, setComments] = useState<commentProps>();
+  const commentHandler = async () => {
+    const res = await fetch("http://localhost:3000/api/comments", {
+      method: "GET",
+      headers: { ticketId: props.id },
+    });
+    if (!res.ok) {
+      console.log("error");
+    }
+    const data = await res.json();
+    setComments(() => data);
+  };
+
+  useEffect(() => {
+    commentHandler();
+  }, []);
+
+  console.log(comments);
   return (
-    <div className="flex max-h-[40rem] flex-col items-center justify-start rounded bg-base-300 p-4 font-bold shadow-lg xl:w-[40%]">
-      <h3 className="m-2 text-4xl  text-primary-content">Discussion</h3>
-      <div className="scrollbar bordered my-4 overflow-y-auto rounded-l bg-base-300">
-        <Comment
-          name="Sarah"
-          time="2:30pm"
-          message="Testing the comment ui Testing the comment uiTesting the comment ui Testing the comment ui"
-        />
-        <Comment
-          name="Sarah"
-          time="2:30pm"
-          message="Testing the comment ui Testing the comment uiTesting the comment ui Testing the comment ui"
-        />
-        <Comment
-          name="Sarah"
-          time="2:30pm"
-          message="Testing the comment ui Testing the comment uiTesting the comment ui Testing the comment ui"
-        />{" "}
-        <Comment
-          name="Sarah"
-          time="2:30pm"
-          message="Testing the comment ui Testing the comment uiTesting the comment ui Testing the comment ui"
-        />{" "}
-        <Comment
-          name="Sarah"
-          time="2:30pm"
-          message="Testing the comment ui Testing the comment uiTesting the comment ui Testing the comment ui"
-        />{" "}
-        <Comment
-          name="Sarah"
-          time="2:30pm"
-          message="Testing the comment ui Testing the comment uiTesting the comment ui Testing the comment ui"
-        />
+    <div className="scrollbar bordered flex h-full min-w-[40%] flex-col items-center justify-between overflow-y-auto rounded rounded-l bg-base-300 p-8 font-bold shadow-lg xl:w-[40%] ">
+      <h3 className="mb-5 items-start justify-start text-center text-4xl text-primary-content">
+        Discussion
+      </h3>
+      <div className="flex h-full w-full flex-col items-start justify-between font-bold ">
+        <div className="">
+          {comments &&
+            comments.map((el: commentProps) => {
+              return (
+                <Comment
+                  key={el.id}
+                  name={el.name}
+                  message={el.message}
+                  time={el.date}
+                />
+              );
+            })}
+        </div>
+        <Commentinput id={props.id} />
       </div>
-      <Commentinput />
     </div>
   );
 };
