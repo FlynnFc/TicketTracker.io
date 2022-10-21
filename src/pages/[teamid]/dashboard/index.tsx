af4767ca72adeb47929403683cb351b579a53d0f
Flynn Foley-Clarke <mastare.flynn@gmail.com>
2022-10-21 19:16:20 +0100

    Top bar of info done on dashboard

--- src/pages/[teamid]/dashboard/index.tsx
+++ src/pages/[teamid]/dashboard/index.tsx
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Miniticket from "../../../components/miniticket/Miniticket";
 import Navbar from "../../../components/navbar/Navbar";
 import { useSession } from "next-auth/react";
+import Drawer from "../../../components/drawer/Drawer";
+import {
+  AiOutlineClose,
+  AiOutlineArrowRight,
+  AiOutlineProject,
+} from "react-icons/ai";
+import { HiOutlineTicket } from "react-icons/hi";
+import Iconbigger from "../../../components/IconBigger/IconBigger";
 
 export async function getServerSideProps() {
   const res = await fetch("https://www.tickettracker.io/api/tickets");
@@ -9,6 +17,7 @@ export async function getServerSideProps() {
     console.log("error");
   }
   const data = await res.json();
+
   return {
     props: {
       ticketprop: data,
@@ -17,6 +26,8 @@ export async function getServerSideProps() {
 }
 
 type newTicketProps = {
+  forEach(arg0: (el: any) => void): unknown;
+  length: number;
   map(
     arg0: (el: {
       priority: string;
@@ -34,40 +45,105 @@ type newTicketProps = {
 };
 
 const Index = (props: { ticketprop: newTicketProps }) => {
+  const [showClose, setShowClose] = useState(false);
+  const [totalTickets, setTotalTickets] = useState(0);
+  const [completedTickets, setCompletedTickets] = useState(0);
   const { data: session } = useSession();
-  console.log(props.ticketprop);
+  useEffect(() => {
+    setTotalTickets(() => props.ticketprop.length);
+  }, [props.ticketprop]);
+  useEffect(() => {
+    const closedFinder = async () => {
+      const res2 = await fetch("http://localhost:3000/api/closedtickets", {
+        method: "GET",
+      });
+      if (!res2.ok) {
+        console.log("error");
+      }
+      const data2 = await res2.json();
+      setCompletedTickets(() => data2.length);
+    };
+    closedFinder();
+  }, []);
   return (
     <>
       {session ? (
-        <div>
-          <Navbar />
-          <section
-            id="minitickets"
-            className="fixed bottom-2 left-2 flex w-full flex-row justify-start"
-          >
-            {props.ticketprop.map(
-              (el: {
-                priority: string;
-                description: string;
-                title: string;
-                id: string;
-              }) => {
-                return (
-                  <Miniticket
-                    key={el.id}
-                    title={el.title}
-                    priority={el.priority}
-                    id={el.id}
-                  />
-                );
-              }
-            )}
-          </section>
-        </div>
-      ) : (
         <>
           <Navbar />
-          <h3 className="text-center text-3xl font-bold">{`You don't have permission to acsess this page`}</h3>
+          <div className="drawer-mobile drawer">
+            <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+            <div className="drawer-content mt-20 ml-4 flex flex-col">
+              <section className="flex space-x-4">
+                <div className="flex flex-row items-center justify-center space-y-2 rounded bg-primary p-4 text-primary-content">
+                  <div className="mr-8 flex flex-col">
+                    <h4 className="text-2xl">Project Status</h4>
+                    <span className="text-3xl font-bold">45%</span>
+                  </div>
+                  <Iconbigger>
+                    <AiOutlineProject />
+                  </Iconbigger>
+                </div>
+                <div className="flex flex-row items-center justify-center space-y-2 rounded bg-info  p-4 text-primary-content">
+                  <div className="mr-8 flex flex-col">
+                    <h4 className="text-2xl">Total Active Tickets</h4>
+                    <span className="text-3xl font-bold">{totalTickets}</span>
+                  </div>
+                  <Iconbigger>
+                    <HiOutlineTicket />
+                  </Iconbigger>
+                </div>
+                <div className="flex flex-row items-center justify-center space-y-2 rounded bg-secondary p-4 text-primary-content">
+                  <div className="mr-8 flex flex-col">
+                    <h4 className="text-2xl">Completed Tickets</h4>
+                    <span className="text-3xl font-bold">
+                      {completedTickets}
+                    </span>
+                  </div>
+                  <Iconbigger>
+                    <HiOutlineTicket />
+                  </Iconbigger>
+                </div>
+              </section>
+              <section
+                id="minitickets"
+                className="fixed bottom-1 hidden w-full flex-row justify-start md:flex"
+              >
+                {props.ticketprop.map(
+                  (el: {
+                    priority: string;
+                    description: string;
+                    title: string;
+                    id: string;
+                  }) => {
+                    return (
+                      <Miniticket
+                        key={el.id}
+                        title={el.title}
+                        priority={el.priority}
+                        id={el.id}
+                      />
+                    );
+                  }
+                )}
+              </section>
+            </div>
+            <label
+              onClick={() => {
+                setShowClose((prev) => !prev);
+              }}
+              htmlFor="my-drawer-2"
+              className={`btn ${
+                showClose ? "btn-error" : "btn-primary"
+              } drawer-button fixed bottom-6 right-6 z-40 lg:hidden`}
+            >
+              {showClose ? <AiOutlineClose /> : <AiOutlineArrowRight />}
+            </label>
+            <Drawer />{" "}
+          </div>
+        </>
+      ) : (
+        <>
+          <h3 className="text-center text-3xl font-bold text-primary-content">{`You don't have permission to acsess this page`}</h3>
         </>
       )}
     </>
