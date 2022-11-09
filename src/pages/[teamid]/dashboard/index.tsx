a243515cb21e4b5f256449dbffcf34a36ca3c450
Flynn Foley-Clarke <mastare.flynn@gmail.com>
2022-11-09 01:42:38 +0000

    Update index.tsx

--- src/pages/[teamid]/dashboard/index.tsx
+++ src/pages/[teamid]/dashboard/index.tsx
@@ -71,99 +71,89 @@ const Index = (props: { ticketprop: newTicketProps }) => {
 
   return (
     <>
-      {session ? (
-        <>
-          <div className="drawer-mobile drawer">
-            <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-            <div className="drawer-content mt-20 ml-4 flex flex-col justify-between">
-              <section className="z-10 flex space-x-4 lg:static">
-                <div className="flex flex-row items-center justify-center space-y-2 rounded bg-primary p-4 text-primary-content">
-                  <div className="mr-8 flex flex-col">
-                    <h4 className="text-2xl">Project Status</h4>
-                    <span className="text-3xl font-bold">45%</span>
-                  </div>
-                  <Iconbigger>
-                    <AiOutlineProject />
-                  </Iconbigger>
-                </div>
-                <div className="flex flex-row items-center justify-center space-y-2 rounded bg-info  p-4 text-primary-content">
-                  <div className="mr-8 flex flex-col">
-                    <h4 className="text-2xl">Total Active Tickets</h4>
-                    <span className="text-3xl font-bold">{totalTickets}</span>
-                  </div>
-                  <Iconbigger>
-                    <HiOutlineTicket />
-                  </Iconbigger>
-                </div>
-                <div className="flex flex-row items-center justify-center space-y-2 rounded bg-secondary p-4 text-primary-content">
-                  <div className="mr-8 flex flex-col">
-                    <h4 className="text-2xl">Completed Tickets</h4>
-                    <span className="text-3xl font-bold">
-                      {completedTickets}
-                    </span>
-                  </div>
-                  <Iconbigger>
-                    <HiOutlineTicket />
-                  </Iconbigger>
-                </div>
-              </section>
-              <section className="z-0 flex h-full w-full flex-col items-center justify-center lg:flex-row lg:justify-start lg:space-x-4">
-                <MyResponsiveBar
-                  totalTickets={totalTickets}
-                  completedTickets={completedTickets}
-                  Active={2}
-                  qa={3}
-                />
-                <div
-                  className="tooltip box-border h-full w-full"
-                  data-tip="currently using seed data"
-                >
-                  <TicketTypeBar data={ticketType} />
-                </div>
-              </section>
-
-              <section
-                id="minitickets"
-                className="hidden w-full flex-row justify-start md:flex"
-              >
-                {props.ticketprop.map(
-                  (el: {
-                    priority: string;
-                    description: string;
-                    title: string;
-                    id: string;
-                  }) => {
-                    return (
-                      <Miniticket
-                        key={el.id}
-                        title={el.title}
-                        priority={el.priority}
-                        id={el.id}
-                      />
-                    );
-                  }
-                )}
-              </section>
+      <div className="drawer drawer-mobile">
+        <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+        <div className="drawer-content mt-20 ml-4 flex flex-col justify-between">
+          <section className="z-10 flex space-x-4 lg:static">
+            <div className="flex flex-row items-center justify-center space-y-2 rounded bg-primary p-4 text-primary-content">
+              <div className="mr-8 flex flex-col">
+                <h4 className="text-2xl">Project Status</h4>
+                <span className="text-3xl font-bold">45%</span>
+              </div>
+              <Iconbigger>
+                <AiOutlineProject />
+              </Iconbigger>
             </div>
-            <label
-              onClick={() => {
-                setShowClose((prev) => !prev);
-              }}
-              htmlFor="my-drawer-2"
-              className={`btn ${
-                showClose ? "btn-error" : "btn-primary"
-              } drawer-button fixed bottom-6 right-6 z-40 lg:hidden`}
+            <div className="flex flex-row items-center justify-center space-y-2 rounded bg-info  p-4 text-primary-content">
+              <div className="mr-8 flex flex-col">
+                <h4 className="text-2xl">Total Active Tickets</h4>
+                <span className="text-3xl font-bold">{totalTickets}</span>
+              </div>
+              <Iconbigger>
+                <HiOutlineTicket />
+              </Iconbigger>
+            </div>
+            <div className="flex flex-row items-center justify-center space-y-2 rounded bg-secondary p-4 text-primary-content">
+              <div className="mr-8 flex flex-col">
+                <h4 className="text-2xl">Completed Tickets</h4>
+                <span className="text-3xl font-bold">{completedTickets}</span>
+              </div>
+              <Iconbigger>
+                <HiOutlineTicket />
+              </Iconbigger>
+            </div>
+          </section>
+          <section className="z-0 flex h-full w-full flex-col items-center justify-center lg:flex-row lg:justify-start lg:space-x-4">
+            <MyResponsiveBar
+              totalTickets={totalTickets}
+              completedTickets={completedTickets}
+              Active={2}
+              qa={3}
+            />
+            <div
+              className="tooltip box-border h-full w-full"
+              data-tip="currently using seed data"
             >
-              {showClose ? <AiOutlineClose /> : <AiOutlineArrowRight />}
-            </label>
-            <Drawer />
-          </div>
-        </>
-      ) : (
-        <>
-          <h3 className="text-center text-3xl font-bold text-primary-content">{`You don't have permission to acsess this page`}</h3>
-        </>
-      )}
+              <TicketTypeBar data={ticketType} />
+            </div>
+          </section>
+
+          <section
+            id="minitickets"
+            className="hidden w-full flex-row justify-start md:flex"
+          >
+            {props.ticketprop.map(
+              (el: {
+                priority: string;
+                description: string;
+                title: string;
+                id: string;
+              }) => {
+                return (
+                  <Miniticket
+                    key={el.id}
+                    title={el.title}
+                    priority={el.priority}
+                    id={el.id}
+                  />
+                );
+              }
+            )}
+          </section>
+        </div>
+        <label
+          onClick={() => {
+            setShowClose((prev) => !prev);
+          }}
+          htmlFor="my-drawer-2"
+          className={`btn ${
+            showClose ? "btn-error" : "btn-primary"
+          } drawer-button fixed bottom-6 right-6 z-40 lg:hidden`}
+        >
+          {showClose ? <AiOutlineClose /> : <AiOutlineArrowRight />}
+        </label>
+        <Drawer />
+      </div>
     </>
   );
 };
